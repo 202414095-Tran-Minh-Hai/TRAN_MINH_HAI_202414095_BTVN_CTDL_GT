18ec1e11b5872d789a693fc9ca7b68cf7d5f36df
202414095-Tran-Minh-Hai <Hai.tm2414095@sis.hust.edu.vn>
2026-04-15 00:20:39 +0700

    tao da thuc  bang danh sach moc noi

--- main.cpp
+++ main.cpp
@@ -1,1 +1,47 @@
-// em chào cô ạ
+// em chào cô ạ   
+#include <iostream>;
+using namespace std;
+// mô tả số hạng
+struct sohang {
+	float heso;
+	int somu;
+	sohang* next;
+};
+// hàm tạo ra số hạng mới
+sohang* taosohang(float a, int b) {
+	sohang* sohangmoi = new sohang;
+	sohangmoi->heso = a;
+	sohangmoi->somu = b;
+	sohangmoi->next = nullptr;
+	return sohangmoi;
+}
+
+// hàm thêm số hạng vào đa thức thứ tự mũ giảm dần
+void themsohang(sohang*& head, float a, int b) {
+	if (a == 0) return; // he so = 0 khoong them
+	sohang* sohangmoi = taosohang(a, b); // tạo ra chuỗi số hạng chứa các hệ số và số mũ 
+	// chèn vào đâu danh sách
+	if (head == nullptr || b > head->somu) {
+		sohangmoi->next = head;
+		head = sohangmoi;
+	}
+	
+	// chèn giữa
+	else {
+		sohang* hientai = head;
+		while (hientai->next != nullptr && hientai->next->somu >= b) {
+			hientai = hientai->next;
+		}
+
+		// mux bằng thì cộng hệ số
+		if (hientai->somu == b) {
+			hientai->heso += a;
+			delete sohangmoi; // xóa vì số hạng mới đã cộng vào số hạng hiện tại
+
+		}
+		else {
+			sohangmoi->next = hientai->next;
+			hientai->next = sohangmoi;
+		}
+	}
+}
